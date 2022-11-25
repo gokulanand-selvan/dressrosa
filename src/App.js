c0695f1d90a7f3bceff1dd2282c21b5148db54c6
gokulanand-selvan <gokul2848@gmail.com>
2022-11-25 23:20:16 +0530

    removed bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import { Box } from '@chakra-ui/react';
-import CardImage from './components/CardImage';
-import GifCard from './components/GifCard';
-import ImageCollage from './components/ImageCollage';
-import img1 from "../src/components/images/img1.jpg";
-import img2 from "../src/components/images/img2.jpg";
-import img3 from "../src/components/images/img3.jpg";
+// import CardImage from './components/CardImage';
+// import GifCard from './components/GifCard';
+// import ImageCollage from './components/ImageCollage';
+// import img1 from "../src/components/images/img1.jpg";
+// import img2 from "../src/components/images/img2.jpg";
+// import img3 from "../src/components/images/img3.jpg";
 // import img5 from "../src/components/images/img5.jpg";
 // import img6 from "../src/components/images/img6.jpg";
 // import img7 from "../src/components/images/img7.jpg";
 import tee1 from '../src/components/images/tee1.jpeg';
 import tee2 from '../src/components/images/tee2.jpeg';
 import Detials from './components/Detials';
-import feature1 from './/components/images/feature1.jpeg';
-import feature2 from './/components/images/feature2.jpeg';
-import feature3 from './/components/images/feature3.jpeg';
-import Slider from './components/Slider';
+// import feature1 from './/components/images/feature1.jpeg';
+// import feature2 from './/components/images/feature2.jpeg';
+// import feature3 from './/components/images/feature3.jpeg';
+// import Slider from './components/Slider';
 import MainPage from './components/MainPage';
  
 
