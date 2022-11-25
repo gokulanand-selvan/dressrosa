31252313a63b7feb8a3c23491760e3183a06d532
gokulanand-selvan <gokul2848@gmail.com>
2022-11-25 09:47:53 +0530

    bug fix

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import { motion } from "framer-motion";
-import Detials from "./Detials";
+// import Detials from "./Detials";
 
 export default function CardImage( {arrayImages, width ,height } ) {
 
